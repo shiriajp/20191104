6afbd269894629f15b4eed87e8be36d5355223f1
shiriajp <57060562+shiriajp@users.noreply.github.com>
2019-11-04 16:17:49 +0900

    add arrow.ts

---
+++ arrow.ts
@@ -0,0 +1,10 @@
+
+const myfunc=(arg:number)=>{
+
+    return arg+1
+}
+
+
+console.log(myfunc(1))
+
+// npm run ts-node arrow.ts
--- helloworld.ts
+++ helloworld.ts
@@ -2,3 +2,20 @@ console.log("helloworld")
 
 // npm run ts-node helloworld.ts
 
+// ls /mnt/c/
+
+// git init
+// git status
+// 
+
+// 57060562+shiriajp@users.noreply.github.com
+
+// https://github.com/shiriajp/20191104.git
+
+// https://github.com/shiriajp
+
+
+// https://qiita.com/sta/items/982ab68e8220a81d485c
+// git config --global user.name shiriajp
+// git config --global user.email 57060562+shiriajp@users.noreply.github.com
+
